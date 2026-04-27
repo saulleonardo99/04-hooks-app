3ef9659554a693f1d31a42575f6bf140ae81b048
Saul Rodriguez <saulrodriguez221299@gmail.com>
2026-04-27 17:38:27 -0600

    feat(zod): implement zod schema validation

--- src/05-useReducer/reducer/tasksReducer.ts
+++ src/05-useReducer/reducer/tasksReducer.ts
@@ -1,3 +1,4 @@
+import * as z from 'zod/v4';
 interface Todo {
     id: number;
     text: string;
@@ -15,13 +16,44 @@ export type TaskAction =
     | { type: 'TOGGLE_TODO', payload: number }
     | { type: 'DELETE_TODO', payload: number };
 
+
+
+const TodoSchema = z.object({
+    id: z.number(),
+    text: z.string(),
+    completed: z.boolean()
+});
+const TaskStateSchema = z.object({
+    todos: z.array(TodoSchema),
+    length: z.number(),
+    completed: z.number(),
+    pending: z.number(),
+});
+
 export const getTaskInitialState = (): TaskState => {
-    return {
-        todos: [],
-        completed: 0,
-        pending: 0,
-        length: 0
+    const localStorageState = localStorage.getItem('task-state');
+    if(!localStorageState){
+        return {
+            todos: [],
+            completed: 0,
+            pending: 0,
+            length: 0
+        }
     }
+
+
+    const result = TaskStateSchema.safeParse(JSON.parse(localStorageState));
+    if(result.error){
+        console.log(result.error);
+         return {
+            todos: [],
+            completed: 0,
+            pending: 0,
+            length: 0
+        }
+    }
+    //! Cuidado, porque el objeto puede haber sido manipulado
+    return result.data;
 }
 export const taskReducer = (
     state: TaskState,

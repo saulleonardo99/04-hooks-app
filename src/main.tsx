834cc9d42fad50fc65b007efadb5b45efeb01cda
Saul Rodriguez <saulrodriguez221299@gmail.com>
2026-04-27 17:41:46 -0600

    feat(ScrambleWords): add funcionality states in component

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { TasksApp } from './05-useReducer/TaskApp'
+import { ScrambleWords } from './05-useReducer/ScrambleWords'
+// import { TasksApp } from './05-useReducer/TaskApp'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { PokemonPage } from './03-examples/PokemonPage'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLightWithHook/> */}
     {/* <PokemonPage/> */}
     {/* <FocusScreen/> */}
-    <TasksApp/>
+    {/* <TasksApp/> */}
+    <ScrambleWords/>
   </StrictMode>,
 )

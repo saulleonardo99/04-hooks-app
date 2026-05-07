9e080c1a03ea642a83778f9c1354db54c07530f0
Saul Rodriguez <saulrodriguez221299@gmail.com>
2026-05-06 18:54:45 -0600

    feat(useOptimistic): create comments section using optimistic hook & useTransition

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { MemoCounter } from './06-memos/MemoCounter'
+import { InstagromApp } from './07-useOptimistic/InstagromApp'
+import { Toaster } from 'sonner'
+
+// import { MemoCounter } from './06-memos/MemoCounter'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { ScrambleWords } from './05-useReducer/ScrambleWords'
 // import { TasksApp } from './05-useReducer/TaskApp'
@@ -14,6 +17,7 @@ import { MemoCounter } from './06-memos/MemoCounter'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster/>
     {/* <HooksApp /> */}
     {/* <TrafficLight></TrafficLight> */}
     {/* <TrafficLightWithEffect/> */}
@@ -23,6 +27,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TasksApp/> */}
     {/* <ScrambleWords/> */}
     {/* <MemoHook/> */}
-    <MemoCounter/>
+    {/* <MemoCounter/> */}
+    <InstagromApp/>
   </StrictMode>,
 )

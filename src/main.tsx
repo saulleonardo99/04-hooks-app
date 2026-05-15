cbf48365ddad88c01ddc879eb8217f546b3fc03d
Saul Rodriguez <saulrodriguez221299@gmail.com>
2026-05-15 15:58:08 -0600

    feat(UseContext): implement Professional App Component to use context

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import { StrictMode, Suspense } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-// import { InstagromApp } from './07-useOptimistic/InstagromApp'
 import { Toaster } from 'sonner'
-import { ClientInformation } from './08-use-suspense/ClientInformation'
-import { getUserAction } from './08-use-suspense/api/get-user.action'
 
+// import { InstagromApp } from './07-useOptimistic/InstagromApp'
+// import { ClientInformation } from './08-use-suspense/ClientInformation'
+// import { getUserAction } from './08-use-suspense/api/get-user.action'
 // import { MemoCounter } from './06-memos/MemoCounter'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { ScrambleWords } from './05-useReducer/ScrambleWords'
@@ -16,6 +15,9 @@ import { getUserAction } from './08-use-suspense/api/get-user.action'
 // import { HooksApp } from './HooksApp'
 // import { TrafficLight } from './01-useState/TrafficLights'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
+import { ProfessionalApp } from './09-useContext/ProfessionalApp'
+
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -31,12 +33,13 @@ createRoot(document.getElementById('root')!).render(
     {/* <MemoHook/> */}
     {/* <MemoCounter/> */}
     {/* <InstagromApp/> */}
-    <Suspense fallback={(
+    {/* <Suspense fallback={(
       <div className="bg-gradient flex flex-col">
         <h1 className="text-white text-2xl">Cargando ...</h1>
       </div>
     )}>
       <ClientInformation getUser={getUserAction(1000)}/>
-    </Suspense>
+    </Suspense> */}
+    <ProfessionalApp/>
   </StrictMode>,
 )
